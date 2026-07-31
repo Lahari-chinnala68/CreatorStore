38867b71abf320ad95b498d93aaeba5448f184ff
Lahari-chinnala68 <laharichinnala68@gmail.com>
2026-07-31 17:15:09 +0530

    footer added

--- frontend/creatorstore-frontend/src/app/app.ts
+++ frontend/creatorstore-frontend/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
 import { Footer } from './footer/footer';
+import { Products } from './products/products';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Products ,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
